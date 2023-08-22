cbd77c9b69a32d640a5373aa0c183ce60626c83b
MerisBaiacu <mariana.carlis@aluno.ifsp.edu.br>
2023-08-22 11:37:33 -0300

    blog

---
+++ includes/rodape.php
@@ -0,0 +1,7 @@
+<div class="card">
+    <div class="card-body text-center">
+        <p>Copyright - <a href='http://ww.ifsp.edu.br'
+                          target='_blank'>IFSP</a></p>
+
+    </div>
+</div>
